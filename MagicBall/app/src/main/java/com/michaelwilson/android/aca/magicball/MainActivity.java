8c808c5557b6d43a5b1f7dc36e65df049436e7cc
mwilson03 <hogman_19_98@yahoo.com>
2016-09-27 10:40:34 -0500

    magic 8 ball redone, i added a third dice just for fun

--- MagicBall/app/src/main/java/com/michaelwilson/android/aca/magicball/MainActivity.java
+++ MagicBall/app/src/main/java/com/michaelwilson/android/aca/magicball/MainActivity.java
@@ -5,39 +5,45 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
             setSupportActionBar(toolbar);
+
             //dice roll button
             FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
             fab.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View view) {
                     Random rand = new Random();
                     Random rand2 = new Random();
+                    Random rand3 = new Random();
                     int roll1 = rand.nextInt(6)+1;
                     int roll2 = rand2.nextInt(6)+1;
+                    int roll3 = rand3.nextInt(6)+1;
                     TextView myText = (TextView) findViewById(R.id.textView);
-                    String myString = String.valueOf(roll1 + roll2);
+                    String myString = String.valueOf(roll1 + roll2 + roll3);
                     myText.setText(myString);
                     Snackbar.make(view, myString, Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
             });
+
             //8 ball button
             FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
             fab2.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View view) {
                     Random rand = new Random();
@@ -46,64 +52,64 @@ public class MainActivity extends AppCompatActivity {
                     String myString = String.valueOf(text);
                     switch (text) {
                         case 1:
-                            myString = "It is Certain";
+                            myString = "It is Certain!";
                             break;
                         case 2:
-                            myString = "It is decidedly so";
+                            myString = "It is decidedly so!";
                             break;
                         case 3:
-                            myString = "Without a doubt";
+                            myString = "Without a shadow of a doubt!";
                             break;
                         case 4:
-                            myString = "Yes, definitely";
+                            myString = "Yes, most definitely!";
                             break;
                         case 5:
-                            myString = "You may rely on it";
+                            myString = "You may rely on it!";
                             break;
                         case 6:
-                            myString = "As I see it, yes";
+                            myString = "As I see it, absolutely!";
                             break;
                         case 7:
-                            myString = "Most likely";
+                            myString = "Most likely!";
                             break;
                         case 8:
-                            myString = "Most likely";
+                            myString = "Outlook Amazing!";
                             break;
                         case 9:
-                            myString = "Yes";
+                            myString = "Yes!";
                             break;
                         case 10:
-                            myString = "Signs point to yes";
+                            myString = "All signs point to YES!";
                             break;
                         case 11:
-                            myString = "Reply hazy try again";
+                            myString = "Reply way to hazy please try again";
                             break;
                         case 12:
-                            myString = "Ask again later";
+                            myString = "Ask again later tater";
                             break;
                         case 13:
-                            myString = "Better not tell you now";
+                            myString = "Better not say!";
                             break;
                         case 14:
-                            myString = "Cannot predict now";
+                            myString = "I cannot predict now, shake me harder!";
                             break;
                         case 15:
-                            myString = "Concentrate and ask again";
+                            myString = "Concentrate real hard and ask me again later!";
                             break;
                         case 16:
-                            myString = "Don't count on it";
+                            myString = "Don't count on it!";
                             break;
                         case 17:
-                            myString = "My reply is no";
+                            myString = "NEIN!!! NEIN!!!!";
                             break;
                         case 18:
-                            myString = "My sources say no";
+                            myString = "My sources say NEIN!";
                             break;
                         case 19:
-                            myString = "Outlook not so good";
+                            myString = "Outlook not so good!";
                             break;
                         case 20:
-                            myString = "Very doubtful";
+                            myString = "Don't be foolish!";
                             break;
                     }
                     myText.setText(myString);
@@ -112,22 +118,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+
     }
